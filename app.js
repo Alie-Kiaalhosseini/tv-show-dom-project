ff1ab44ad017096c815bd49d88017c71ba32e5a4
Alie-Kiaalhosseini <aliesadat.kia2000@gmail.com>
2022-02-17 13:27:18 +0330

    style main part

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
 const main = document.querySelector('main');
 const select = document.querySelector('select');
 const search = document.querySelector('form input');
+//fetch data 
 const fetchingData = async () =>{
     const request = await fetch('https://api.tvmaze.com/shows/48450/episodes');
     const data =  await request.json();
     return data;
 };
-// const makeCard2 = (id)
+//make card for each object
 const makeCard = (episode) =>{
     const card = document.createElement('section');
     const a = document.createElement('a');
     const img = document.createElement('img');
-    // card.classList.add('display');
     card.setAttribute('id',episode.id);
     img.setAttribute('src',episode.image.medium);
     a.setAttribute('href',episode.url);
     a.appendChild(img);
     card.appendChild(a);
     card.innerHTML+=`<h5>${episode.name} , S0${episode.season}E0${episode.number}</h5>
-    <div>${episode.summary}</div>`;
+    <div><span><i class="bi bi-clock-fill clock"></i>${episode.airtime}</span>
+    <div class="summary"><i class=" bi bi-binoculars-fill "></i></div>${episode.summary}</div>`;
     main.appendChild(card);
 };
+//make option for each object
 const makeOptionList = (episode) =>{
     const option = document.createElement('option');
     option.setAttribute('value',episode.id);
     option.innerText = `S0${episode.season}E0${episode.number} - ${episode.name}`;
     select.appendChild(option);
 };
+//filter card whenever press key
 const filterMovies = (term) => {
     Array.from(main.children)
     .filter((episode) => !episode.querySelector('h5').textContent.toLowerCase().includes(term))
@@ -35,10 +38,12 @@ const filterMovies = (term) => {
     .filter((episode) => episode.querySelector('h5').textContent.toLowerCase().includes(term))
     .forEach((episode) => episode.classList.remove('filtered'));
 }
+//whenever press key call fuction to filter card
 search.addEventListener('keyup', e => {
     const term = search.value.trim().toLowerCase();
     filterMovies(term);
 });
+//whenever you click on option list it shows that card
 select.addEventListener('change',(e)=>{
     const id = e.target.value;
     if(id !== 'all'){
@@ -56,6 +61,7 @@ select.addEventListener('change',(e)=>{
     }
 });
 
+//call data 
 fetchingData()
 .then(episodes =>{
     console.log(episodes);
